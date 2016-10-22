d7d318743d2d548ee9ee93acffef8a06b4e4c136
Kami Jean <kamijeanwilson@gmail.com>
2016-10-22 08:20:27 -0700

    Initial commit with instructions

---
+++ app.js
@@ -0,0 +1,7 @@
+// This will be where the file you call to start the project e.g. (node app.js)
+
+// Require the profile file
+
+// Get the argument from the command line which will be called like--node app.js kamijean--the last part is the argument
+
+// Call to the profile function that will call the ajax function
---
+++ profile.js
@@ -0,0 +1,15 @@
+// require http
+
+// Function to print out message
+
+// Function to print out error messages
+
+// Function to connect to the API URL ( https://api.github.com/users/{username} )
+
+// Read the data
+
+// Parse the data
+
+// Print the data
+
+// Output the Error
